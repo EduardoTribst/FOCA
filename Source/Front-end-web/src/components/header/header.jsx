7c29646b1746000514c42c89c3ad8aab66a0ecd8
EduardoTribst <eduardo.tribst@gmail.com>
2026-03-26 11:50:02 -0300

    inicio do component de header

---
+++ Source/Front-end-web/src/components/header/header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+        <div>
+            //icon
+            <h1>FOCAS</h1>
+        </div>
+      
+    </div>
+  );
+}
+
+export default Header;
