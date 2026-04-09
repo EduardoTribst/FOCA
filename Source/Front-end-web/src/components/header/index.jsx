bbd9bd9c63c78069d2d1ec09ff6ae05542e2ceea
EduardoTribst <eduardo.tribst@gmail.com>
2026-04-09 10:50:40 -0300

    assets novos incluidos e componente de header corrigido

--- Source/Front-end-web/src/components/header/index.jsx
+++ Source/Front-end-web/src/components/header/index.jsx
@@ -1,48 +1,68 @@
 import "./header.css";
 import { useNavigate } from "react-router-dom";
-import logo from "../../assets/foca.svg";
+import logo from "../../assets/seal.svg";
+import UserIcon from "../../assets/user.svg?react";
+import HomeIcon from "../../assets/home.svg?react";
+import InfoIcon from "../../assets/info.svg?react";
 
 export default function Header({
-  title = "FOCA",
-  links = [],
-  textButton,
-  routeButton,
-  showButton = false,
+	routes = [
+		{ textButton: "Início", routeButton: "/" },
+		{ textButton: "Sobre o Projeto", routeButton: "/" },
+		{ textButton: "Perfil", routeButton: "/perfil" },
+	],
 }) {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  function goTo(route) {
-    navigate(route);
-  }
+	function goTo(route) {
+		navigate(route);
+	}
 
-  return (
-    <header className="header">
-      <div className="header-left">
-            <img src={logo} alt="Logo" className="header-logo" />
-            <h1 className="header-title">{title}</h1>
-      </div>
+	return (
+		<header className="header">
+			<div className="header-elements">
+				<div className="header-left">
 
+					<img src={logo} alt="Logo" className="header-logo" />
+					<h1 className="header-title">FOCA</h1>
 
-      <div className="header-right">
-        {links.map((link, index) => (
-          <button
-            key={index}
-            className="header-link"
-            onClick={() => goTo(link.routeButton)}
-          >
-            {link.textButton}
-          </button>
-        ))}
+				</div>
 
-        {showButton && (
-          <button
-            className="header-button"
-            onClick={() => goTo(routeButton)}
-          >
-            {textButton}
-          </button>
-        )}
-      </div>
-    </header>
-  );
+				<div className="header-right">
+
+					<button
+						className="header-link"
+						onClick={() => goTo(routes[0].routeButton)}
+					>
+						<div className="content-button">
+							<HomeIcon className="home-icon" />
+							{routes[0].textButton}
+						</div>
+					</button>
+
+					<button
+						className="header-link"
+						onClick={() => goTo(routes[1].routeButton)}
+					>
+						<div className="content-button">
+							<InfoIcon className="info-icon" />
+							{routes[1].textButton}
+						</div>
+
+					</button>
+
+					<button
+						className="header-button"
+						onClick={() => goTo(routes[2].routeButton)}
+					>
+						<div className="content-button">
+							<UserIcon className="user-icon" />
+							{routes[2].textButton}
+						</div>
+
+					</button>
+				</div>
+			</div>
+		</header>
+	);
 }
--- Source/Front-end-web/src/inicial/index.jsx
+++ Source/Front-end-web/src/inicial/index.jsx
@@ -1,21 +1,15 @@
-import { useNavigate } from 'react-router-dom';
 import Header from '../components/header';
-import logoFoca from "../assets/foca.svg";
 
 export default function Inicial(){
-    const navigate = useNavigate()
-
-    function irParaLogin(){
-        navigate("/login")
-    }
 
     return(
         <div>
             <Header
-            titulo="FOCA"
-            links={[
-                { texto: "Início", destino: "/" },
-                { texto: "Criar conta", destino: "/cadastro" }
+            login={true}
+            routes={[
+                { textButton: "Início", routeButton: "/" },
+                { textButton: "Sobre o Projeto", routeButton: "/" },
+                { textButton: "Login", routeButton: "/login" },
             ]}
             />
             <h1>Tela Inicial</h1>
